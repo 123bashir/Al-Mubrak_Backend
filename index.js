17e2e58ebc25b532b45aa042e2ac883ad3f8b75c
bashfortech <141651763+123bashir@users.noreply.github.com>
2025-12-03 14:06:54 +0100

    cloudinary updates

--- index.js
+++ index.js
@@ -77,9 +77,9 @@ import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "du4laiwmj",
+  api_key: "567859429173318",
+  api_secret: "csPinc3oHfZcXzyEU0rHNrQD9z4",
 });
 
 const storage = new CloudinaryStorage({
